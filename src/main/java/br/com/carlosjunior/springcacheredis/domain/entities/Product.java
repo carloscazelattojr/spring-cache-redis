7bc73520ddff34e3250a9b53a9cbcb07127d1416
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2023-06-19 23:16:39 -0300

    implementando redis

--- src/main/java/br/com/carlosjunior/springcacheredis/domain/entities/Product.java
+++ src/main/java/br/com/carlosjunior/springcacheredis/domain/entities/Product.java
@@ -6,17 +6,32 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.Generated;
 import org.hibernate.annotations.NotFound;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+import org.springframework.util.SerializationUtils;
 
 import java.io.Serializable;
 
 @Entity
 @Table(name = "product")
 @Data
-public class Product implements Serializable {
+public class Product implements RedisSerializer<Product> {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
+
+    @Override
+    public byte[] serialize(Product product) throws SerializationException {
+        return SerializationUtils.serialize(product);
+    }
+
+    @Override
+    public Product deserialize(byte[] bytes) throws SerializationException {
+        return (Product) SerializationUtils.deserialize(bytes);
+    }
 }
